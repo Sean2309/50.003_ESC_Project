c59663903506641ab946cc4d1f3c2049634b5460
unknown <miyamii.x@gmail.com>
2023-07-15 17:57:47 +0800

    feat: added easy debug createUser function to add profile and credentials to db, refactor: changed checking for email to checking for loginId

--- bank-app/server/controllers/authManagerController.js
+++ bank-app/server/controllers/authManagerController.js
@@ -1,15 +1,19 @@
-const User = require('../models/user');
+const UserCredentials = require('../models/userCredentials');
+const UserProfile = require('../models/userProfile');
 
 class AuthManagerController {
+  constructor() {
+    this.createUser();
+  }
 
-// Handle the POST request to authenticate a user
+  // Handle the POST request to authenticate a user
   userAuthentication = async (request, response) => {
     try {
-      const { email, password } = request.body;
+      const { loginId, password } = request.body;
 
       // Find the user by email
       // TODO: Implement security features, e.g. hash password
-      User.findOne({ email })
+      UserCredentials.findOne({ loginId: loginId })
         .then(user => {
           if (user) {
             if (user.password == password) {
@@ -25,6 +29,31 @@ class AuthManagerController {
       console.error(error);
       response.status(500).json({ message: 'Server error' });
     }
+  }
+
+  // createUser for easy debug and call it during instantiation
+  createUser = async (request, response) => {
+    await UserCredentials.deleteMany({});
+    await UserProfile.deleteMany({});
+
+    const userCredentials = new UserCredentials({
+      loginId: 'john123',
+      password: 'password',
+      userId: '1'
+    });
+
+    await userCredentials.save();
+
+    const userProfile = new UserProfile({
+      firstName: 'John',
+      lastName: 'Yeet',
+      abcPoints: 10000,
+      emailAddress: 'johnyeet@gmail.com',
+      phoneNumber: '88889912',
+      userId: userCredentials.userId
+    });
+    
+    await userProfile.save();
   }
 }
 
