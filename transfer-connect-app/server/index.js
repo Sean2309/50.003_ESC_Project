f361e7bba1ff697e8a7334b49931b183c271e760
TengTjinYang <101085998+TengTjinYang@users.noreply.github.com>
2023-07-18 16:34:46 +0800

    index.js updated

--- transfer-connect-app/server/index.js
+++ transfer-connect-app/server/index.js
@@ -1,8 +1,16 @@
 // npm install cron
 var CronJob = require('cron').CronJob;
-var queryFromDBandUpload = require('controllers/accrualFileController').queryFromDBandUpload;
+var queryFromDBandUpload = require('./controllers/accrualFileController').queryFromDBandUpload;
 var job = new CronJob(
     '0 0 0 * * *',
     queryFromDBandUpload,
 );
-// job.start() - See note below when to use this
+job.start() // - See note below when to use this
+// Seconds (0-59)
+// Minutes (0-59)
+// Hours (0-23)
+// Day of Month (1-31)
+// Months (0-11, January is 0, December is 11)
+// Day of Week (0-6, Sunday is 0, Saturday is 6)
+// The asterisk (*) means "any value" or "every" in cron syntax. 
+// A value of 0 means exactly at the 0th second, 0th minute, or 0th hour.
