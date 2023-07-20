1290d8bdabedb799dd5be2cece9a78a602bca08c
Daniel Yuen <daniel_yuen@mymail.sutd.edu.sg>
2023-07-21 00:49:08 +0800

    TransactionValidation: now able to check membershipId format

--- transfer-connect-app/server/controllers/validateTransaction.js
+++ transfer-connect-app/server/controllers/validateTransaction.js
@@ -1,26 +1,12 @@
-const validateTransaction = (req, res, next) => {
+const LoyaltyProgramQueryModel = require('../models/loyaltyProgramQueryModel');
 
-
-  /*
-{
-  membershipId: String,
-  memberName: String,
-  transferDate: String,
-  transferAmount: String,
-  emailAddress: String,
-  phoneNumber: String,
-  notificationMethod: String,
-  referenceNumber: Integer
-  partnerCode: String,
-}
-*/
-
+const validateTransaction = async (req, res, next) => {
   const reqBody = req.body;
-  const requiredFields = ['membershipId', 'memberName','transferDate','emailAddress','phoneNumber','notificationMethod','referenceNumber','partnerCode'];
+  const requiredFields = ['membershipId', 'memberName', 'transferDate', 'emailAddress', 'phoneNumber', 'notificationMethod', 'referenceNumber', 'partnerCode'];
   const missingFields = requiredFields.filter(field => !reqBody[field]);
-  if (missingFields.length > 0 ) {
-      return res.status(400).json({ error: `Missing required fields: ${missingFields.join(', ')}` });
-  };
+  if (missingFields.length > 0) {
+    return res.status(400).json({ error: `Missing required fields: ${missingFields.join(', ')}` });
+  }
 
   const fieldTypes = {
     membershipId: 'string',
@@ -38,8 +24,33 @@ const validateTransaction = (req, res, next) => {
   if (typeErrors.length > 0) {
     return res.status(400).json({ error: `Invalid data types for fields: ${typeErrors.join(', ')}` });
   }
-  next();
 
+  // Retrieve the loyalty program by its identifier (e.g., programId) from the database
+  const loyaltyProgramId = req.params.loyaltyProgramId;
+  try {
+    const loyaltyProgram = await LoyaltyProgramQueryModel.findOne({ programId: loyaltyProgramId }).exec();
+    console.log("Inside validateTransaction(): LoyaltyProgram");
+    console.log(loyaltyProgram);
+    if (!loyaltyProgram) {
+      return res.status(404).json({ error: "Loyalty program not found." });
+    }
+
+    // Use the membershipFormat from the loyalty program to construct the regex for membershipId
+    const membershipIdRegexFromDB = new RegExp(loyaltyProgram.membershipFormat);
+    console.log("Inside validateTransaction(): membershipIdRegexFromDB");
+    console.log(membershipIdRegexFromDB);
+
+    if (!membershipIdRegexFromDB.test(reqBody.membershipId)) {
+      return res.status(400).json({ error: "Invalid membershipId format for this loyalty program." });
+    }
+
+    
+  } catch (error) {
+    console.error('Error retrieving loyalty program data:', error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
+
+  next();
 };
 
 module.exports = validateTransaction;
