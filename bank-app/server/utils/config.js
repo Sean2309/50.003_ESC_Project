399e7dbf8172342910a401589fe7ec47c271b229
unknown <miyamii.x@gmail.com>
2023-07-21 16:10:08 +0800

    chore: added cookie-parser, added SECRET_CODE export

--- bank-app/server/utils/config.js
+++ bank-app/server/utils/config.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const MONGODB_URL = process.env.MONGODB_URL
 const PORT = process.env.PORT
 const TRANSFER_CONNECT_API_URL = process.env.TRANSFER_CONNECT_API_URL
+const SECRET_CODE = process.env.SECRET_CODE
 
 module.exports = {
-    MONGODB_URL, PORT, TRANSFER_CONNECT_API_URL
+    MONGODB_URL, PORT, TRANSFER_CONNECT_API_URL, SECRET_CODE
 }
