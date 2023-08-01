a7c49945def0f4009985d242fe560d3c30b36cc3
unknown <miyamii.x@gmail.com>
2023-08-02 00:36:29 +0800

    style: styled source code according to eslint, chore: installed eslint

--- bank-app/server/routes/transferFormRouter.js
+++ bank-app/server/routes/transferFormRouter.js
@@ -1,8 +1,8 @@
 const transferFormRouter = require('express').Router();
 const transferFormController = require('../controllers/transferFormController');
-const validateTransferForm = require('../utils/validateTransferForm');
+const validateTransaction = require('../utils/validateTransaction');
 
 // Route for creating a new transfer form, with path parameter to different loyaltyProgram
-transferFormRouter.post('/:loyaltyProgramId', validateTransferForm, transferFormController.submitTransferForm)
+transferFormRouter.post('/:loyaltyProgramId', validateTransaction, transferFormController.submitTransferForm);
 
 module.exports = transferFormRouter;
