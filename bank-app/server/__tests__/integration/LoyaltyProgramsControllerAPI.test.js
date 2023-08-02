adde037bf68f4a90f09cde2ac2c4a91b9763a5d2
Ong Zheng Han <ozhenghan2@gmail.com>
2023-08-03 00:30:46 +0800

    chore: move test file

---
+++ bank-app/server/__tests__/integration/LoyaltyProgramsControllerAPI.test.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const loyaltyProgramsController = require('../../controllers/loyaltyProgramsController');
+const LoyaltyPrograms = require('../../models/loyaltyPrograms');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const request = require('supertest');
+// Mock Axios to simulate the HTTP GET request/response
+jest.mock('axios');
+app.listen = jest.fn(() => ({
+    close: jest.fn(),
+  }));
+
+app.get('/api/loyaltyPrograms', (req, res) => {
+    res.json({ loyaltyPrograms: ['Program A', 'Program B'] });
+  });
+
+// =========== Test Suite and Cases ======== //
+describe('LoyaltyProgramsController - API Integration', () => {
+    test('api/loyaltyPrograms should respond with 200 status code', async () => {
+        const response = await request(app).get('/api/loyaltyprograms');
+        expect(response.status).toBe(200);
+        // expect(response.body).toHaveProperty('loyaltyPrograms');
+        // expect(response.body.loyaltyPrograms).toHaveLength(2);
+    
+});
+  });z
