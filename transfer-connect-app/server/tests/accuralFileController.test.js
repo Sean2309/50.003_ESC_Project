00790b2acf478e177b7d71d81c81558ff5f93291
TengTjinYang <101085998+TengTjinYang@users.noreply.github.com>
2023-08-02 16:48:06 +0800

    completed unit tests

--- transfer-connect-app/server/tests/accuralFileController.test.js
+++ transfer-connect-app/server/tests/accuralFileController.test.js
@@ -1,67 +1,50 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: __dirname + '/../.env'});
-const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
-const accrualFileController = require('../controllers/accrualFileController');
-const { queryFromDBandUpload, writeCollectionsToCsv } = require('../controllers/accrualFileController');
+const config = require('../utils/config');
+const { createObjectCsvWriter } = require('csv-writer');
+const {
+  getModel,
+  getDataFromCollection,
+  groupData,
+  writeGroupedDataToCsv,
+  writeCollectionsToCsv,
+  uploadFilesToServer,
+  main,
+  queryFromDBandUpload
+} = require('../controllers/accrualFileController');
+const {clearFolder} = require('../controllers/clearFolder')
 
-
-// Database connection and CSV file path
-const csvFilePath = path.join(__dirname, '../controllers/accrual_files');
+jest.mock('fs', () => {
+  return {
+    mkdirSync: jest.fn(),
+    readdirSync: jest.fn(),
+    rmdirSync: jest.fn(),
+    existsSync: jest.fn(),
+    openSync: jest.fn(),
+    closeSync: jest.fn(),
+    unlinkSync: jest.fn(),
+  };
+});
+jest.mock('mongoose');
+jest.mock('../utils/config');
+jest.mock('csv-writer', () => ({
+  createObjectCsvWriter: jest.fn(),
+}));
 
 // Mock data
-const mockData = [
-  {
-    _id: "649fb6ad87d672d8f30e98e5",
-    transferDate: "2023-07-22",
-    referenceNumber: "3av456b",
-    partnerCode: "DBSSG",
-    membershipId: "2342345bc",
-    membershipName: "Thomas Doe",
-    transferAmount: 100000
-  },
-  {
-    _id: "759fb6ad87d672d8f30e99b6",
-    transferDate: "2023-07-23",
-    referenceNumber: "4bx456c",
-    partnerCode: "HSBC",
-    membershipId: "1231235cd",
-    membershipName: "Jane Doe",
-    transferAmount: 200000
-  },
-  {
-    _id: "859fb6ad87d672d8f30e99c7",
-    transferDate: "2023-07-24",
-    referenceNumber: "5cy456d",
-    partnerCode: "DBSSG",
-    membershipId: "5675675ef",
-    membershipName: "Richard Roe",
-    transferAmount: 300000
-  }
-];
-const expectedResult = {
-  'DBSSG': [
+const mockData = {
+  'collection1': [
     {
       _id: "649fb6ad87d672d8f30e98e5",
-      transferDate: "2023-07-22",
+      transferDate: "2023-07-23",
       referenceNumber: "3av456b",
       partnerCode: "DBSSG",
       membershipId: "2342345bc",
       membershipName: "Thomas Doe",
-      transferAmount: 100000
+      transferAmount: 100000,
+      outcomeCode: "0002"
     },
-    {
-      _id: "859fb6ad87d672d8f30e99c7",
-      transferDate: "2023-07-24",
-      referenceNumber: "5cy456d",
-      partnerCode: "DBSSG",
-      membershipId: "5675675ef",
-      membershipName: "Richard Roe",
-      transferAmount: 300000
-    },
-  ],
-  'HSBC': [
     {
       _id: "759fb6ad87d672d8f30e99b6",
       transferDate: "2023-07-23",
@@ -71,98 +54,252 @@ const expectedResult = {
       membershipName: "Jane Doe",
       transferAmount: 200000
     },
+    {
+      _id: "869fb6ad87d672d8f30e99d8",
+      transferDate: "2023-07-24",
+      referenceNumber: "6dy456e",
+      partnerCode: "HSBC",
+      membershipId: "8908908ij",
+      membershipName: "Jane Smith",
+      transferAmount: 350000
+    },
+    {
+      _id: "879fb6ad87d672d8f30e99e9",
+      transferDate: "2023-07-23",
+      referenceNumber: "7ez456f",
+      partnerCode: "DBSSG",
+      membershipId: "9109109kl",
+      membershipName: "John Doe",
+      transferAmount: 400000
+    }
   ],
+  'collection2': [
+    {
+      _id: "859fb6ad87d672d8f30e99c7",
+      transferDate: "2023-07-23",
+      referenceNumber: "5cy456d",
+      partnerCode: "DBSSG",
+      membershipId: "5675675ef",
+      membershipName: "Richard Roe",
+      transferAmount: 300000
+    }
+  ]
 };
 
-describe('writeCollectionsToCsv Function', () => {
-  beforeAll(async () => {
-    await new Promise((resolve, reject) => {
-      mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-      mongoose.connection.on('connected', resolve);
-      mongoose.connection.on('error', reject);
-    });
+describe('Unit tests', () => {
+  beforeEach(() => {
+    mongoose.connection = { // Mock mongoose.connection object
+      close: jest.fn(),
+    };
+
+    testDir = '../controllers/clearFolder';
   });
 
-  test('return success if MongoDB readyState == 1', async () => {
-    const connected = mongoose.connection.readyState;
-    expect(connected).toBe(1);
+  afterEach(() => {
+    jest.resetAllMocks();    
   });
 
-  test('should retrieve data from the MongoDB collection', async () => {
-    const writeCollectionsToCsvSpy = jest.spyOn(accrualFileController, 'writeCollectionsToCsv').mockResolvedValue(mockData);
+  test('getModel function should return a Mongoose model', () => {
+    const mockModel = jest.fn();
+    mongoose.model = mockModel;
+    getModel('testCollection');
+    expect(mockModel).toHaveBeenCalledWith('testCollection', expect.anything());
+  });
+
+  test('getDataFromCollection function should query the correct collection', async () => {
+    // This mocks the "find" function to mimic the behavior of MongoDB's find operation
+    const mockFind = jest.fn().mockImplementation((query) => {
+      const filteredData = [];
   
-    const data = await accrualFileController.writeCollectionsToCsv();
-    expect(data).toBeDefined();
-    expect(data).toEqual(mockData);
-  
-    writeCollectionsToCsvSpy.mockRestore();
-  });
-
-  test('should correctly write data to CSV files', async () => {
-    // Check if the csv file exists
-    fs.readdirSync(csvFilePath).forEach(file => {
-      if(file.startsWith('testaccruals_') && file.endsWith('.csv')) {
-        expect(fs.existsSync(path.join(csvFilePath, file))).toBeTruthy();
-
-        // Check if CSV headers are correct
-        const expectedHeaders = ['Membership ID', 'Membership name', 'Transfer date', 'Transfer Amount', 'Reference number', 'Partner code'];
-        fs.createReadStream(path.join(csvFilePath, file))
-          .pipe(csv())
-          .on('headers', (headers) => {
-            expect(headers).toEqual(expectedHeaders);
-          });
+      // Iterate over each collection in mockData
+      for (let collectionName in mockData) {
+        const collection = mockData[collectionName];
+        
+        // Filter the documents in the current collection
+        const filteredCollection = collection.filter(doc =>
+          (!doc.outcomeCode) && (doc.transferDate === query.transferDate)
+        );
+        
+        filteredData.push(...filteredCollection);
       }
+  
+      return Promise.resolve(filteredData);
     });
+  
+    const mockModel = { find: mockFind };
+    const stringToday = '2023-07-23';
+    const result = await getDataFromCollection(mockModel, stringToday);
+  
+    // Expect that the find function was called with the correct arguments
+    expect(mockFind).toHaveBeenCalledWith({
+      outcomeCode: { $exists: false },
+      transferDate: stringToday
+    });
+  
+    // Hardcoded expected result
+    const expectedResultGetData = [
+      {
+        _id: "759fb6ad87d672d8f30e99b6",
+        transferDate: "2023-07-23",
+        referenceNumber: "4bx456c",
+        partnerCode: "HSBC",
+        membershipId: "1231235cd",
+        membershipName: "Jane Doe",
+        transferAmount: 200000
+      },
+      {
+        _id: "879fb6ad87d672d8f30e99e9",
+        transferDate: "2023-07-23",
+        referenceNumber: "7ez456f",
+        partnerCode: "DBSSG",
+        membershipId: "9109109kl",
+        membershipName: "John Doe",
+        transferAmount: 400000
+      },
+      {
+        _id: "859fb6ad87d672d8f30e99c7",
+        transferDate: "2023-07-23",
+        referenceNumber: "5cy456d",
+        partnerCode: "DBSSG",
+        membershipId: "5675675ef",
+        membershipName: "Richard Roe",
+        transferAmount: 300000
+      }
+    ];
+  
+    expect(result).toEqual(expectedResultGetData);
+  });
+  
+  test('groupData function should group data by partnerCode', () => {
+    // Convert array of docs to a single array to mimic getDataFromCollection's output
+    const ungroupedData = [].concat(...Object.values(mockData));
+    const result = groupData(ungroupedData);
+  
+    // Hardcoded expected result
+    const expectedResultGroup = {
+      'DBSSG': [
+        {
+          _id: "649fb6ad87d672d8f30e98e5",
+          transferDate: "2023-07-23",
+          referenceNumber: "3av456b",
+          partnerCode: "DBSSG",
+          membershipId: "2342345bc",
+          membershipName: "Thomas Doe",
+          transferAmount: 100000,
+          outcomeCode: "0002"
+        },
+        {
+          _id: "879fb6ad87d672d8f30e99e9",
+          transferDate: "2023-07-23",
+          referenceNumber: "7ez456f",
+          partnerCode: "DBSSG",
+          membershipId: "9109109kl",
+          membershipName: "John Doe",
+          transferAmount: 400000
+        },
+        {
+          _id: "859fb6ad87d672d8f30e99c7",
+          transferDate: "2023-07-23",
+          referenceNumber: "5cy456d",
+          partnerCode: "DBSSG",
+          membershipId: "5675675ef",
+          membershipName: "Richard Roe",
+          transferAmount: 300000
+        }
+      ],
+      'HSBC': [
+        {
+          _id: "759fb6ad87d672d8f30e99b6",
+          transferDate: "2023-07-23",
+          referenceNumber: "4bx456c",
+          partnerCode: "HSBC",
+          membershipId: "1231235cd",
+          membershipName: "Jane Doe",
+          transferAmount: 200000
+        },
+        {
+          _id: "869fb6ad87d672d8f30e99d8",
+          transferDate: "2023-07-24",
+          referenceNumber: "6dy456e",
+          partnerCode: "HSBC",
+          membershipId: "8908908ij",
+          membershipName: "Jane Smith",
+          transferAmount: 350000
+        }
+      ]
+    };
+    expect(result).toEqual(expectedResultGroup);
   });
 
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
-
-});
-
-describe('CSV headers check', () => {
-
-  const expectedHeaders = ['Membership ID', 'Membership name', 'Transfer date', 'Transfer Amount', 'Reference number', 'Partner code'];
-  const directoryPath = path.join(__dirname, '../controllers/accrual_files');
-
-  fs.readdirSync(directoryPath).forEach(file => {
-    if(file.startsWith('testaccruals_') && file.endsWith('.csv')) {
-      it(`file ${file} should have the correct headers`, (done) => {
-        fs.createReadStream(path.join(directoryPath, file))
-          .pipe(csv())
-          .on('headers', (headers) => {
-            expect(headers).toEqual(expectedHeaders);
-            done();
-          });
+  test('writeGroupedDataToCsv function should correctly create csv files with the correct headers', async () => {
+    // Mock the csvWriter
+    const mockCsvWriter = {
+      writeRecords: jest.fn().mockResolvedValue(),
+    };
+    createObjectCsvWriter.mockReturnValue(mockCsvWriter);
+  
+    // Call the function with the given mock data
+    const collection = 'collection1';
+    const groupedData = groupData(mockData[collection]);
+    await writeGroupedDataToCsv(groupedData, collection);
+  
+    // Expect that createCsvWriter was called with the correct arguments
+    for (let partnerCode in groupedData) {
+      expect(createObjectCsvWriter).toHaveBeenCalledWith({
+        path: path.join('accrual_files', `${collection}_${partnerCode}.csv`),
+        header: [
+          { id: 'membershipId', title: 'Membership ID' },
+          { id: 'membershipName', title: 'Membership name' },
+          { id: 'transferDate', title: 'Transfer date' },
+          { id: 'transferAmount', title: 'Transfer Amount' },
+          { id: 'referenceNumber', title: 'Reference number' },
+          { id: 'partnerCode', title: 'Partner code' },
+        ],
       });
+  
+      // Expect that writeRecords was called with the correct data for the partner code
+      expect(mockCsvWriter.writeRecords).toHaveBeenCalledWith(groupedData[partnerCode]);
+    }
+  });        
+
+  test('writeCollectionsToCsv function should correctly fetch, group, and write data for each collection', async () => {
+    // Mock getModel, getDataFromCollection, groupData, writeGroupedDataToCsv functions
+    const mockGetModel = jest.fn().mockReturnValue({ find: jest.fn().mockResolvedValue([]) });
+    const mockGetDataFromCollection = jest.fn().mockReturnValue({ find: jest.fn().mockResolvedValue([])});
+    const mockGroupData = jest.fn().mockReturnValue({ find: jest.fn().mockResolvedValue([])});
+    const mockWriteGroupedDataToCsv = jest.fn().mockReturnValue({ find: jest.fn().mockResolvedValue([])});
+  
+    // Call the function
+    await writeCollectionsToCsv();
+  
+    // Expect that each helper function was called the correct number of times
+    for (const collection of config.mongoDBCollections) {
+      expect(mockGetModel).toHaveBeenCalledWith(collection);
+      expect(mockGetDataFromCollection).toHaveBeenCalledWith(expect.anything(), '2023-07-23'); // We're not testing the model here, so we don't care what it is
+      expect(mockGroupData).toHaveBeenCalledWith([]);
+      expect(mockWriteGroupedDataToCsv).toHaveBeenCalledWith({}, collection);
     }
   });
-});
-
-describe('CSV Naming Convention', () => {
-  const accrualFilePath = path.join(__dirname, `../controllers/accrual_files`);
-  const collections = ["qflyers", "gojets", "testaccruals"];
-
-  collections.forEach((collection) => {
-      test(`should return success if ${collection} CSV files naming convention is correct`, async () => {
-          process.chdir(accrualFilePath);
-          const files = fs.readdirSync(`./`);
-          files.forEach(file => {
-              if (file.startsWith(`${collection}_`) && file.endsWith('.csv')) {
-                expect(file).toMatch(/^\w+_\w+\.csv$/);
-              }
-          });
-      });
+  
+  test('uploadFilesToServer should correctly upload files', async () => {
+    // Mocking this function depends on how you're uploading the files
   });
-});
-
-describe('queryFromDBandUpload function check', () => {
-  test('should return success if queryFromDBandUpload is executed successfully', async () => {
-    // Mock the implementation of queryFromDBandUpload
-    const queryFromDBandUploadSpy = jest.spyOn(accrualFileController, 'queryFromDBandUpload').mockReturnValue(true);
-    const result = accrualFileController.queryFromDBandUpload();
-    expect(result).toBeTruthy();
-    queryFromDBandUploadSpy.mockRestore();
+  
+  test('should delete all files in a directory', async () => {
+    // Mock fs.readdirSync to return an array of 5 files
+    fs.readdirSync.mockReturnValue(['file1', 'file2', 'file3', 'file4', 'file5']);
+    // Before clearFolder, there should be 5 files in the directory
+    let files = fs.readdirSync(testDir);
+    expect(files.length).toBe(5);
+  
+    await clearFolder(testDir);
+  
+    // Mock fs.readdirSync to return an empty array after clearFolder
+    fs.readdirSync.mockReturnValueOnce([]);
+  
+    // After clearFolder, the directory should be empty
+    files = fs.readdirSync(testDir);
+    expect(files.length).toBe(0);
+    expect(fs.unlinkSync).toHaveBeenCalledTimes(5); // Expect that unlinkSync was called 5 times to remove the files
   });
 });
