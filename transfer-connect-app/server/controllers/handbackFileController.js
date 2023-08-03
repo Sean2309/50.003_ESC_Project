b907346c71748980edbe90954ffc1d02bf9a7944
keith <miyamii.x@gmail.com>
2023-08-03 17:33:45 +0800

    refactor: refactored controller code into class controllers

--- transfer-connect-app/server/controllers/handbackFileController.js
+++ transfer-connect-app/server/controllers/handbackFileController.js
@@ -1,5 +1,5 @@
 // Importing files/modules
-require('dotenv').config({path: __dirname + '/../.env'});
+require('dotenv').config({ path: __dirname + '/../.env' });
 const transactionEnquiryModel = require('../models/transactionEnquiryModel');
 const mongoose = require('mongoose');
 const fs = require(`fs`);
@@ -7,163 +7,173 @@ const csvParser = require(`csv-parser`);
 const Files = require('files.com/lib/Files').default;
 const File = require('files.com/lib/models/File').default;
 const { isBrowser } = require('files.com/lib/utils');
-const path  = require('path');
+const path = require('path');
 var getFormattedDate = require('./date').getFormattedDate;
 var clearFolder = require('./clearFolder').clearFolder;
 var config = require('../utils/config');
-
-// Defining Variables
-// Dates
-const testDate = `20200812`; 
-const formattedDate = getFormattedDate("compact");
-// SFTP Downloads Folder Name
-const sftpHandbackDownloads = 'sftp_handback_downloads';
-// Cache for storing the created models
-const modelCache = {};
-
-const testMongoDBURL = 'mongodb+srv://tengtjinyang:zagNwPsta2HHTyfE@transferconnect.0papjri.mongodb.net/TransferConnectDB';
-
-// Function to get the model for a given Loyalty Program
-const getModelForLP = (loyaltyProgram) => {
-  if (!modelCache[loyaltyProgram]) {
-    const model = mongoose.models[loyaltyProgram] || mongoose.model(loyaltyProgram, transactionEnquiryModel);
-    modelCache[loyaltyProgram] = model;
-  }
-  return modelCache[loyaltyProgram];
-};
+const { CronJob } = require('cron');
 
 // Creates the sftp_handback_downloads folder if it doesn't exist
 if (!fs.existsSync(path.join(__dirname, 'sftp_handback_downloads'))) {
   fs.mkdirSync(path.join(__dirname, 'sftp_handback_downloads'));
 }
 
+class HandbackFileController {
 
-// =========== START OF MAIN FUNCTIONS ======================
-const retrieveFromServer = async(targetDate) => {
-  /*
-  This function retrieves the handback files from the SFTP server
-  */
-  let fileName;
-  for (const collection of config.sftpCollections ) {
-    // Config Details
-    Files.setBaseUrl(config.kaligoURL);
-    Files.setApiKey(config.kaligoAPIKey);
-    // Downloading the handback file from the server
-    console.log("Retrieving the files from the SFTP server");
+  constructor() {
+    this.formattedDate = getFormattedDate("compact");
+    // SFTP Downloads Folder Name
+    this.sftpHandbackDownloads = 'sftp_handback_downloads';
+    // Cache for storing the created models
+    this.modelCache = {};
+    this.testMongoDBURL = 'mongodb+srv://tengtjinyang:zagNwPsta2HHTyfE@transferconnect.0papjri.mongodb.net/TransferConnectDB';
 
-    fileName = `${collection}_HANDBACK_${targetDate}.csv`;
-    const foundFile = await File.find(`/transfer_connect_sutd_case_study_2023/c4i1/Handback/${collection}/${fileName}`, {mkdir_parents: true});
-    const downloadableFile = await foundFile.download();
-
-    if (!isBrowser()) {
-      // Download to a file on disk
-      await downloadableFile.downloadToFile(path.join(__dirname, `${sftpHandbackDownloads}/${fileName}`));
-      console.log(`File ${fileName} downloaded!\n`);
-    };
-  };
-};
-
-const extractDataFromCsv = async(filePath) => {
-  /*
-  Function tasks:
-  1) extracts data from the downloaded handback csv files
-  2) generates a random outcome code for each 
-
-  */
-
-  return new Promise((resolve, reject) => {
-
-    const str1 = filePath.split('\\');
-    const splitStr = str1[str1.length-1].split(/_/);
-    const partnerCode = splitStr[0];
-
-    const results = []; // List to store the dictionaries
-
-    fs.createReadStream(filePath)
-      .pipe(csvParser())
-      .on('data', (data) => {
-        // Extracting outcome code
-        if (data['Outcome Code'] && data['Outcome Code'].startsWith('"') && data['Outcome Code'].endsWith('"')) {
-          data['Outcome Code'] = data['Outcome Code'].slice(1, -1);
-        };
-
-        // Pushing the whole result
-        results.push(data);
-      })
-      .on('end', () => {
-        console.log(`Extracted Data:`, results);
-        resolve([partnerCode, results]);
-      })
-      .on(`error`, (error) =>  {
-        reject(error);
-      })
-  });
-  
-};
-
-const uploadFilesToMongoDB = async (targetDate) => {
-  await mongoose.connect(testMongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-  for (let i = 0; i < config.sftpCollections.length; i++) {
-    const filePath = path.join(__dirname, `${sftpHandbackDownloads}/${config.sftpCollections[i]}_HANDBACK_${targetDate}.csv`);
-    try {
-      const [partnerCode, results] = await extractDataFromCsv(filePath);
-      // console.log(`Extracting data from ${partnerCode} Handback File`);
-      const Model = getModelForLP(config.mongoDBCollections[i]);
-      for (const result of results) {
-        let mappedResult = {
-          transferDate: result['Transfer date'],
-          partnerCode: partnerCode,
-          referenceNumber: result['Reference number'],
-          outcomeCode: result['Outcome Code'],
-          transferAmount: parseInt(result['Transfer Amount']),
-        };
-        
-        let doc = await Model.findOne({ 
-          $and: [{referenceNumber: mappedResult.referenceNumber},
-          {transferDate: mappedResult.transferDate} ]
-        }); // Must match the referenceNumber and transferDate to update
-        if (doc) {
-          doc.set(mappedResult);
-          console.log(`Data uploaded: `, doc)
-          await doc.save();
-        } else {
-          const newModel = await Model.create(mappedResult);
-          console.log(`new model: `, newModel)
-        }
-      }
-      
-      // console.log(`Data updated for ${partnerCode} successfully\n`);
-    } catch (error) {
-      console.log(error);
-    }
+    this.startService();
   }
 
-  mongoose.connection.close();
-};
+  startService = async () => {
+    let job = new CronJob(
+      '30 30 * * * *',
+      this.downloadfromSFTPandUpload,
+    )
+    job.start();
+  }
 
-// END OF MAIN FUNCTIONS ======================
 
-// Running the functions
-const main = async () => {
-  clearFolder(sftpHandbackDownloads);
-  await retrieveFromServer(testDate);
-  
-  await uploadFilesToMongoDB(testDate);
-  console.log("Done!");
+  downloadfromSFTPandUpload = async () => {
+    clearFolder(this.sftpHandbackDownloads);
+    await retrieveFromServer(this.formattedDate);
+    await uploadFilesToMongoDB(this.formattedDate);
+  }
+
+  testAccrualFileFns = async () => {
+    // Defining Variables
+    // Dates
+    const testDate = `20200812`;
+
+    // Running the functions
+    clearFolder(sftpHandbackDownloads);
+    await retrieveFromServer(testDate);
+
+    await uploadFilesToMongoDB(testDate);
+    console.log("Done!");
+
+  }
+
+  // Function to get the model for a given Loyalty Program
+  getModelForLP = (loyaltyProgram) => {
+    if (!this.modelCache[loyaltyProgram]) {
+      const model = mongoose.models[loyaltyProgram] || mongoose.model(loyaltyProgram, transactionEnquiryModel);
+      this.modelCache[loyaltyProgram] = model;
+    }
+    return this.modelCache[loyaltyProgram];
+  };
+
+  // =========== START OF MAIN FUNCTIONS ======================
+  retrieveFromServer = async (targetDate) => {
+    /*
+    This function retrieves the handback files from the SFTP server
+    */
+    let fileName;
+    for (const collection of config.sftpCollections) {
+      // Config Details
+      Files.setBaseUrl(config.kaligoURL);
+      Files.setApiKey(config.kaligoAPIKey);
+      // Downloading the handback file from the server
+      console.log("Retrieving the files from the SFTP server");
+
+      fileName = `${collection}_HANDBACK_${targetDate}.csv`;
+      const foundFile = await File.find(`/transfer_connect_sutd_case_study_2023/c4i1/Handback/${collection}/${fileName}`, { mkdir_parents: true });
+      const downloadableFile = await foundFile.download();
+
+      if (!isBrowser()) {
+        // Download to a file on disk
+        await downloadableFile.downloadToFile(path.join(__dirname, `${sftpHandbackDownloads}/${fileName}`));
+        console.log(`File ${fileName} downloaded!\n`);
+      };
+    };
+  };
+
+  extractDataFromCsv = async (filePath) => {
+    /*
+    Function tasks:
+    1) extracts data from the downloaded handback csv files
+    2) generates a random outcome code for each 
+   
+    */
+
+    return new Promise((resolve, reject) => {
+
+      const str1 = filePath.split('\\');
+      const splitStr = str1[str1.length - 1].split(/_/);
+      const partnerCode = splitStr[0];
+
+      const results = []; // List to store the dictionaries
+
+      fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on('data', (data) => {
+          // Extracting outcome code
+          if (data['Outcome Code'] && data['Outcome Code'].startsWith('"') && data['Outcome Code'].endsWith('"')) {
+            data['Outcome Code'] = data['Outcome Code'].slice(1, -1);
+          };
+
+          // Pushing the whole result
+          results.push(data);
+        })
+        .on('end', () => {
+          console.log(`Extracted Data:`, results);
+          resolve([partnerCode, results]);
+        })
+        .on(`error`, (error) => {
+          reject(error);
+        })
+    });
+
+  };
+
+  uploadFilesToMongoDB = async (targetDate) => {
+
+    for (let i = 0; i < config.sftpCollections.length; i++) {
+      const filePath = path.join(__dirname, `${sftpHandbackDownloads}/${config.sftpCollections[i]}_HANDBACK_${targetDate}.csv`);
+      try {
+        const [partnerCode, results] = await extractDataFromCsv(filePath);
+        // console.log(`Extracting data from ${partnerCode} Handback File`);
+        const Model = getModelForLP(config.mongoDBCollections[i]);
+        for (const result of results) {
+          let mappedResult = {
+            transferDate: result['Transfer date'],
+            partnerCode: partnerCode,
+            referenceNumber: result['Reference number'],
+            outcomeCode: result['Outcome Code'],
+            transferAmount: parseInt(result['Transfer Amount']),
+          };
+
+          let doc = await Model.findOne({
+            $and: [{ referenceNumber: mappedResult.referenceNumber },
+            { transferDate: mappedResult.transferDate }]
+          }); // Must match the referenceNumber and transferDate to update
+          if (doc) {
+            doc.set(mappedResult);
+            console.log(`Data uploaded: `, doc)
+            await doc.save();
+          } else {
+            const newModel = await Model.create(mappedResult);
+            console.log(`new model: `, newModel)
+          }
+        }
+
+        // console.log(`Data updated for ${partnerCode} successfully\n`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+  };
+  // END OF MAIN FUNCTIONS ======================
+
 }
-main().catch(console.error);
 
+const handbackFileController = new HandbackFileController();
 
-const downloadfromSFTPandUpload = async () => {
-  clearFolder(sftpHandbackDownloads);
-  await retrieveFromServer(formattedDate);
-  await uploadFilesToMongoDB(formattedDate); 
-};
-
-module.exports = {
-  retrieveFromServer,
-  extractDataFromCsv,
-  uploadFilesToMongoDB,
-}
-module.exports.downloadfromSFTPandUpload = downloadfromSFTPandUpload;
+module.exports = handbackFileController;
--- transfer-connect-app/server/controllers/transactionEnquiryController.js
+++ transfer-connect-app/server/controllers/transactionEnquiryController.js
@@ -1,5 +1,5 @@
 const transactionSchema = require('../models/transactionEnquiryModel.js');
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 const emailNotification = require('./emailNotification.js');
 const messageNotification = require('./messageNotification.js');
 
@@ -7,14 +7,14 @@ const messageNotification = require('./messageNotification.js');
 //can improve code by using caching for faster data retrieval
 class TransactionEnquiryController {
 
-  constructor(){
+  constructor() {
   }
 
   //processRoute
-  processRoute = async (req, res) =>{
+  processRoute = async (req, res) => {
     const id = req.params;
-    if (id == null){
-        return;
+    if (id == null) {
+      return;
     }
     console.log(id.loyalty_program);
     console.log(id.bank_app);
@@ -30,7 +30,7 @@ class TransactionEnquiryController {
     } else {
       collection_connection = mongoose.model(loyalty_program_name, transactionSchema, loyalty_program_name);
     }
-    
+
     //pass in reference numbers
     const id_list = id.systemId.split(",");
     console.log(id_list);
@@ -41,11 +41,11 @@ class TransactionEnquiryController {
   }
 
 
-  getOutcomeCode = async (collection_connection, id_list, bank_name, loyalty_program_name) =>{
+  getOutcomeCode = async (collection_connection, id_list, bank_name, loyalty_program_name) => {
     console.log(id_list);
     let outcomeCodes = [];
     //use of instead of in - in makes 0000 into 0 
-    for (let id of id_list){
+    for (let id of id_list) {
       console.log(id);
       let user = await this.find_transaction(collection_connection, id, bank_name);
       if (user[0] != null) {
@@ -53,23 +53,23 @@ class TransactionEnquiryController {
         console.log('Found transactions:', user);
         outcomeCodes.push(user[0]);
         this.sendNotification(user1.phoneNumber, user1.emailAddress, user1.notificationMethod, user1.outcomeCode, bank_name, loyalty_program_name, user1.transferAmount);
-      } 
+      }
       else {
         console.log('Outcome code not updated or transaction not found.');
       }
     }
     return outcomeCodes;
-    };
+  };
 
-  find_transaction = async(collection_connection, id, bank_name) => {
+  find_transaction = async (collection_connection, id, bank_name) => {
     //use .lean().exec() to return an obj instead of document
     //check if systemId has outcomeCode field + not empty
     try {
-    console.log("find_transaction")
-    console.log(id)
-    let user = await collection_connection.find({"systemId": id, "outcomeCode":{$exists: true, $ne:""}, "partnerCode": bank_name}, {"outcomeCode": 1, "systemId": 1, "_id": 0, "notificationMethod": 1, "phoneNumber": 1, "emailAddress" : 1, "transferAmount" : 1, "membershipId": 1 }).lean().exec()
-    return user;
-    } catch(error) {
+      console.log("find_transaction")
+      console.log(id)
+      let user = await collection_connection.find({ "systemId": id, "outcomeCode": { $exists: true, $ne: "" }, "partnerCode": bank_name }, { "outcomeCode": 1, "systemId": 1, "_id": 0, "notificationMethod": 1, "phoneNumber": 1, "emailAddress": 1, "transferAmount": 1, "membershipId": 1 }).lean().exec()
+      return user;
+    } catch (error) {
       //can change this to throw status code
       console.log(error)
       return error
@@ -77,24 +77,24 @@ class TransactionEnquiryController {
   }
 
 
-  sendNotification = async (phoneNumber, email, notificationMethod, outcomeCode, bank_name, loyalty_program_name, transferAmount) =>{
-    if (notificationMethod == 0){
+  sendNotification = async (phoneNumber, email, notificationMethod, outcomeCode, bank_name, loyalty_program_name, transferAmount) => {
+    if (notificationMethod == 0) {
       //only email
       console.log('sent email');
       emailNotification.sendEmail(email, bank_name, loyalty_program_name, outcomeCode, transferAmount);
     }
-    else if (notificationMethod == 1){
+    else if (notificationMethod == 1) {
       //only phone number
       console.log("sent message")
-      messageNotification.sendMessages(phoneNumber, bank_name,loyalty_program_name, outcomeCode, transferAmount);
+      messageNotification.sendMessages(phoneNumber, bank_name, loyalty_program_name, outcomeCode, transferAmount);
     }
-    else{
+    else {
       //both
       console.log('sent email');
       emailNotification.sendEmail(email, bank_name, loyalty_program_name, outcomeCode, transferAmount);
 
       console.log("sent message")
-      messageNotification.sendMessages(phoneNumber, bank_name,loyalty_program_name, outcomeCode, transferAmount);
+      messageNotification.sendMessages(phoneNumber, bank_name, loyalty_program_name, outcomeCode, transferAmount);
     }
   }
 
