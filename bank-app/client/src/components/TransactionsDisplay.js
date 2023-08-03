109fc6e9156d6993d2375ef1ffcb3390ac58554f
unknown <miyamii.x@gmail.com>
2023-08-04 04:52:31 +0800

    Revert "fix: partial fixing of duplication"
    This reverts commit f0486f1638cf87d5dbd3f5fd45390420be99668e.

--- bank-app/client/src/components/TransactionsDisplay.js
+++ bank-app/client/src/components/TransactionsDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Transaction from './Transaction';
 import axios from 'axios';
 
@@ -7,9 +7,8 @@ const TransactionsDisplay = (props) => {
 
   const [transactions, setTransactions] = useState({});
   const [componentsArray, setComponentsArray] = useState([]);
-  const [uniqueTransactionRefs, setUniqueTransactionRefs] = useState(new Set());
 
-  const fetchTransactions = async () => {
+  const getTransactions = async () => {
     try {
       const transactionEnquiryResponse = await axios.get(`http://localhost:3001/api/transactions/${userId}`);
       const transactionEnquiryData = transactionEnquiryResponse.data;
@@ -19,34 +18,29 @@ const TransactionsDisplay = (props) => {
     }
   };
 
-  useLayoutEffect(() => {
-    // Fetch transactions when the component is mounted
-    fetchTransactions();
+  useEffect(() => {
+    getTransactions();
   }, [userId]);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     // Call the renderTransactions function to create the components array
+    setComponentsArray(renderTransactions());
+  }, [transactions]);
+
+  const renderTransactions = () => {
     const array = [];
 
     // Loop through each transaction array along with loyaltyProgramId as key, and map them to Transaction components
     Object.entries(transactions).forEach(([key, transactionArray]) => {
-      const transactionsRendered = transactionArray.map((transaction) => {
-        if (!uniqueTransactionRefs.has(transaction.referenceNumber)) {
-          // Push the transaction ID to the set to prevent duplicates
-          setUniqueTransactionRefs((prevSet) => new Set(prevSet).add(transaction.referenceNumber));
-
-          return (
-            <Transaction key={transaction.systemId} transaction={transaction} loyaltyProgramId={key} />
-          );
-        }
-        return null; // Skip duplicate transactions
-      });
+      const transactionsRendered = transactionArray.map((transaction) => (
+        <Transaction key={transaction.systemId} transaction={transaction} loyaltyProgramId={key}/>
+      ));
       array.push(...transactionsRendered);
     });
 
-    // Update the components array
-    setComponentsArray(array);
-  }, [transactions, uniqueTransactionRefs]);
+    // Return the array of Transaction components
+    return array;
+  };
 
   return <div>{Object.keys(transactions).length > 0 && componentsArray}</div>;
 };
