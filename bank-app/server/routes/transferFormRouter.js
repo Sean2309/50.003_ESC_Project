a084521cda78223f080d1f78bfb148f68ed98ff6
keith <miyamii.x@gmail.com>
2023-08-07 22:32:01 +0800

    feat: added mutex middleware to ensure mutex on transaction submission route endpoint

--- bank-app/server/routes/transferFormRouter.js
+++ bank-app/server/routes/transferFormRouter.js
@@ -2,9 +2,20 @@ const transferFormRouter = require('express').Router();
 const transferFormController = require('../controllers/transferFormController');
 const validateTransaction = require('../utils/validateTransaction');
 const userProfileController = require('../controllers/userProfileController');
+const { Mutex } = require('async-mutex');
+const routeMutex = new Mutex();
+
+const mutexMiddleware = (request, response, next) => {
+    routeMutex.acquire().then(release => {
+    response.on('finish', release); // Release the mutex lock when the response is finished
+    next();
+  });
+}
+
 
 // Route for creating a new transfer form, with path parameter to different loyaltyProgram
 transferFormRouter.post('/:loyaltyProgramId',
+    mutexMiddleware,
     userProfileController.authenticateToken,
     validateTransaction,
     transferFormController.submitTransferForm,
