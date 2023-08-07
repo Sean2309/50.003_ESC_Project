438e2654d50a3cbc030422be2af465977839e52f
MichelleCDjunaidi <87893361+MichelleCDjunaidi@users.noreply.github.com>
2023-08-07 14:33:53 +0800

    test: added fuzzing and close form testing

--- bank-app/client/src/test/unit/TransferForm.test.js
+++ bank-app/client/src/test/unit/TransferForm.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, fireEvent, screen, act, waitFor } from '@testing-library/react';
 // import axiosMock from './axiosMock'; // Mock axios for testing purposes
 import TransferForm from '../../components/TransferForm';
+import StringFuzzer from '../StringFuzzer';
 import axios from 'axios';
-
 
 jest.mock('axios');
 
@@ -151,5 +151,205 @@ describe('TransferForm Component', () => {
 
       // getDate was called
       await expect(spy).toHaveBeenCalled();
+    });
+
+    it('fuzzing random format membershipId (membershipIdConfirmation is the same value)', async () => {
+
+      const fuzzer = new StringFuzzer(mockedLoyaltyProgramData.membershipFormat, mockedUserProfile.abcPoints);
+      const fuzzMembershipId = fuzzer.generateRandomMembershipId(Math.random()*50);
+      const regex = new RegExp(mockedLoyaltyProgramData.membershipFormat);
+  
+      // since it is not clicked yet, this is still the button at the end of the loyalty program card
+      await act(async () => {
+        render(<TransferForm 
+          membershipFormat={mockedLoyaltyProgramData.membershipFormat}
+          currencyRate={mockedLoyaltyProgramData.currencyRate}
+          userProfile={mockedUserProfile}
+          loyaltyProgramId={mockedLoyaltyProgramData.programId}
+          />);
+      });
+        
+        // https://stackoverflow.com/questions/66043164/testing-click-event-in-react-testing-library
+        const transferButton = screen.getByRole('button');
+        // user opens transferForm
+        fireEvent.click(transferButton);
+  
+        // User finds input fields and submit button
+        const memberNameInput = screen.getByTestId('member-name').querySelector('input');
+        const membershipIdInput = screen.getByTestId('member-id').querySelector('input');
+        const membershipIdConfirmationInput = screen.getByTestId('member-confirm').querySelector('input');
+        const transferAmountInput = screen.getByTestId('transfer-amount').querySelector('input');
+        // const submitButton = screen.getByTestId('submit-button');
+        const submitForm = screen.getByTestId('submit-form');
+  
+        // User fills in the form
+        fireEvent.change(memberNameInput, { target: { value: 'John Doe' } });
+        // invalid membershipId
+        fireEvent.change(membershipIdInput, { target: { value: fuzzMembershipId } });
+        fireEvent.change(membershipIdConfirmationInput, { target: { value: fuzzMembershipId } });
+        fireEvent.change(transferAmountInput, { target: { value: '50' } });
+  
+        // User presses the submit button on the form
+        // console.log(fuzzMembershipId);
+        submitForm.submit();
+        await waitFor(() => {
+            if (regex.test(fuzzMembershipId) == false) {
+              expect(axios.post).not.toHaveBeenCalled();
+            }
+            else if (regex.test(fuzzMembershipId) == true) {
+              expect(axios.post).toHaveBeenCalled();
+            }
+          });
+    });
+
+    it('fuzzing random format membershipId and membershipIdConfirmation', async () => {
+
+      const fuzzer = new StringFuzzer(mockedLoyaltyProgramData.membershipFormat, mockedUserProfile.abcPoints);
+      const fuzzMembershipId = fuzzer.generateRandomMembershipId(Math.random()*50);
+      const fuzzMembershipIdConfirmation = fuzzer.generateRandomMembershipId(Math.random()*50);
+      const regex = new RegExp(mockedLoyaltyProgramData.membershipFormat);
+  
+      // since it is not clicked yet, this is still the button at the end of the loyalty program card
+      await act(async () => {
+        render(<TransferForm 
+          membershipFormat={mockedLoyaltyProgramData.membershipFormat}
+          currencyRate={mockedLoyaltyProgramData.currencyRate}
+          userProfile={mockedUserProfile}
+          loyaltyProgramId={mockedLoyaltyProgramData.programId}
+          />);
+      });
+        
+      // https://stackoverflow.com/questions/66043164/testing-click-event-in-react-testing-library
+      const transferButton = screen.getByRole('button');
+      // user opens transferForm
+      fireEvent.click(transferButton);
+
+      // User finds input fields and submit button
+      const memberNameInput = screen.getByTestId('member-name').querySelector('input');
+      const membershipIdInput = screen.getByTestId('member-id').querySelector('input');
+      const membershipIdConfirmationInput = screen.getByTestId('member-confirm').querySelector('input');
+      const transferAmountInput = screen.getByTestId('transfer-amount').querySelector('input');
+      // const submitButton = screen.getByTestId('submit-button');
+      const submitForm = screen.getByTestId('submit-form');
+
+      // User fills in the form
+      fireEvent.change(memberNameInput, { target: { value: 'John Doe' } });
+      // invalid membershipId
+      fireEvent.change(membershipIdInput, { target: { value: fuzzMembershipId } });
+      fireEvent.change(membershipIdConfirmationInput, { target: { value: fuzzMembershipIdConfirmation } });
+      fireEvent.change(transferAmountInput, { target: { value: '50' } });
+
+      // User presses the submit button on the form
+      // console.log(fuzzMembershipId);
+      submitForm.submit();
+      await waitFor(() => {
+        if (regex.test(fuzzMembershipId) == false || fuzzMembershipId!=fuzzMembershipIdConfirmation) {
+          expect(axios.post).not.toHaveBeenCalled();
+        }
+        // tbh this can just be an else, but it's clearer this way
+        else if (regex.test(fuzzMembershipId) == true && fuzzMembershipId==fuzzMembershipIdConfirmation) {
+          expect(axios.post).toHaveBeenCalled();
+        }
+      });
+    });
+
+    it('fuzzing transfer amount input, allowing invalid inputs such as alphabets', async () => {
+
+      const fuzzer = new StringFuzzer(mockedLoyaltyProgramData.membershipFormat, mockedUserProfile.abcPoints);
+      const fuzzTransferAmountInput = fuzzer.generateRandomPointInput(Math.random()*50);
+      // number only checker
+      const regex = new RegExp("^\\[0-9]+$");
+  
+      // since it is not clicked yet, this is still the button at the end of the loyalty program card
+      await act(async () => {
+        render(<TransferForm 
+          membershipFormat={mockedLoyaltyProgramData.membershipFormat}
+          currencyRate={mockedLoyaltyProgramData.currencyRate}
+          userProfile={mockedUserProfile}
+          loyaltyProgramId={mockedLoyaltyProgramData.programId}
+          />);
+      });
+        
+        // https://stackoverflow.com/questions/66043164/testing-click-event-in-react-testing-library
+        const transferButton = screen.getByRole('button');
+        // user opens transferForm
+        fireEvent.click(transferButton);
+  
+        // User finds input fields and submit button
+        const memberNameInput = screen.getByTestId('member-name').querySelector('input');
+        const membershipIdInput = screen.getByTestId('member-id').querySelector('input');
+        const membershipIdConfirmationInput = screen.getByTestId('member-confirm').querySelector('input');
+        const transferAmountInput = screen.getByTestId('transfer-amount').querySelector('input');
+        // const submitButton = screen.getByTestId('submit-button');
+        const submitForm = screen.getByTestId('submit-form');
+  
+        // User fills in the form
+        fireEvent.change(memberNameInput, { target: { value: 'John Doe' } });
+        // invalid membershipId
+        fireEvent.change(membershipIdInput, { target: { value: '123456789S' } });
+        fireEvent.change(membershipIdConfirmationInput, { target: { value: '123456789S' } });
+        fireEvent.change(transferAmountInput, { target: { value: fuzzTransferAmountInput } });
+  
+        // User presses the submit button on the form
+        console.log(fuzzTransferAmountInput);
+        submitForm.submit();
+        await waitFor(() => {
+            if (regex.test(fuzzTransferAmountInput) == false || parseInt(fuzzTransferAmountInput)>mockedUserProfile.abcPoints) {
+              expect(axios.post).not.toHaveBeenCalled();
+            }
+            else if (regex.test(fuzzTransferAmountInput) == true && parseInt(fuzzTransferAmountInput)<=mockedUserProfile.abcPoints) {
+              expect(axios.post).toHaveBeenCalled();
+            }
+          });
+    });
+
+    it('fuzzing transfer amount input, only integer values', async () => {
+
+      const fuzzer = new StringFuzzer(mockedLoyaltyProgramData.membershipFormat, mockedUserProfile.abcPoints);
+      const fuzzTransferAmountInput = fuzzer.generateRandomIntegerInput(Math.random()*50);
+      // number only checker
+      const regex = new RegExp("^\\[0-9]+$");
+  
+      // since it is not clicked yet, this is still the button at the end of the loyalty program card
+      await act(async () => {
+        render(<TransferForm 
+          membershipFormat={mockedLoyaltyProgramData.membershipFormat}
+          currencyRate={mockedLoyaltyProgramData.currencyRate}
+          userProfile={mockedUserProfile}
+          loyaltyProgramId={mockedLoyaltyProgramData.programId}
+          />);
+      });
+        
+        // https://stackoverflow.com/questions/66043164/testing-click-event-in-react-testing-library
+        const transferButton = screen.getByRole('button');
+        // user opens transferForm
+        fireEvent.click(transferButton);
+  
+        // User finds input fields and submit button
+        const memberNameInput = screen.getByTestId('member-name').querySelector('input');
+        const membershipIdInput = screen.getByTestId('member-id').querySelector('input');
+        const membershipIdConfirmationInput = screen.getByTestId('member-confirm').querySelector('input');
+        const transferAmountInput = screen.getByTestId('transfer-amount').querySelector('input');
+        // const submitButton = screen.getByTestId('submit-button');
+        const submitForm = screen.getByTestId('submit-form');
+  
+        // User fills in the form
+        fireEvent.change(memberNameInput, { target: { value: 'John Doe' } });
+        // invalid membershipId
+        fireEvent.change(membershipIdInput, { target: { value: '123456789S' } });
+        fireEvent.change(membershipIdConfirmationInput, { target: { value: '123456789S' } });
+        fireEvent.change(transferAmountInput, { target: { value: fuzzTransferAmountInput } });
+  
+        // User presses the submit button on the form
+        console.log(fuzzTransferAmountInput);
+        submitForm.submit();
+        await waitFor(() => {
+            if (parseInt(fuzzTransferAmountInput)>mockedUserProfile.abcPoints) {
+              expect(axios.post).not.toHaveBeenCalled();
+            }
+            else if (parseInt(fuzzTransferAmountInput)<=mockedUserProfile.abcPoints) {
+              expect(axios.post).toHaveBeenCalled();
+            }
+          });
     });
 });
